c764544de610cad85fb5febc876a9794f5111ff4
Koi McCormick <koi_mccormick@yahoo.com>
2024-07-18 03:42:28 -0700

    Update to README and beginning game text

--- ProfessorVoltChallenge/src/main/java/com/mankyller/Game.java
+++ ProfessorVoltChallenge/src/main/java/com/mankyller/Game.java
@@ -2,13 +2,6 @@ package com.mankyller;
 
 public class Game {
 
-    private final String DESCRIPTION = "It's graduation day at Clearwater Academy, and excitement is in the air." +
-            "But there's a twist! Dr. Volt, your eccentric science teacher, has always emphasized the importance of" +
-            "practical knowledge. Today, he's decided to put that philosophy to the ultimate test. Just hours before the " +
-            "ceremony, Dr. Volt has locked you and your classmates in the science lab and created a secret 4-digit" +
-            "code that you must use to escape.";
-
-
 
     public static void main(String[] args) {
         Game game = new Game();
@@ -16,7 +9,14 @@ public class Game {
     }
 
     public void beginGame() {
-        System.out.println(DESCRIPTION);
+        System.out.println("MISSION: Escape the Science Lab!");
+        System.out.println("Dr. Volt has created a series of challenges that cover everything you learned this" +
+                "year. ");
+        System.out.println("Dr. Volt: 'Only those who truly understand the essence of science and" +
+                "mathematics deserve to graduate. You have one hour to prove your worth or the doors" +
+                "remain locked, and you'll miss walking across the graduation stage! MUAHAHAHAHAHAHA!!'  ");
+        System.out.println("Can you solve Dr. Volt's puzzles and crack the code in time? Or will you" +
+                "miss the special moment you've worked hard for all these years? TIME IS TICKING!");
         System.out.println("Press 1 to begin: ");
 
 
